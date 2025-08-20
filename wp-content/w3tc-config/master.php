767d55c344917970c9fadfc20a0bd4b18674b8ab
Andor Salamon <salamon.andor@gmail.com>
2025-08-20 16:27:42 +0200

    Daily update

--- wp-content/w3tc-config/master.php
+++ wp-content/w3tc-config/master.php
@@ -1,5 +1,5 @@
 <?php exit; ?>{
-    "version": "2.8.11",
+    "version": "2.8.12",
     "cluster.messagebus.debug": false,
     "cluster.messagebus.enabled": false,
     "cluster.messagebus.sns.region": "",
